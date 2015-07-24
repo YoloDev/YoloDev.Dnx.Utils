816d405060b4c6c3206643793c63391885301c1a
Alxandr <alxandr@alxandr.me>
2015-07-24 02:15:24 +0200

    add constructors

--- sample/NativeSample/Class1.cs
+++ sample/NativeSample/Class1.cs
@@ -1,10 +1,21 @@
 using System.Runtime.InteropServices;
+using Microsoft.Framework.Runtime;
 using YoloDev.Dnx.NativeUtils;
 
 namespace NativeSample
 {
+
     abstract class FooBar : NativeMethods
     {
+        public class TestAttribute : System.Attribute {}
+        readonly IRuntimeEnvironment _env;
+
+        [Test]
+        public FooBar(IRuntimeEnvironment env)
+        {
+            _env = env;
+        }
+
         [NativeMethod("somelib", CallingConvention.Cdecl, BestFitMapping = false)]
         public abstract void Test();
 
